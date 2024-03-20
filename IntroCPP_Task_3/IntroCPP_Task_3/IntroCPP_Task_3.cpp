60cfc689a0850a8448e65f6d0e516c5eb6312513
Deion Trinidad <deion_trinidad@outlook.com>
2024-03-20 22:58:12 +1100

    Writing Code to Implement Test Log

--- IntroCPP_Task_3/IntroCPP_Task_3/IntroCPP_Task_3.cpp
+++ IntroCPP_Task_3/IntroCPP_Task_3/IntroCPP_Task_3.cpp
@@ -1,23 +1,82 @@
 #include "String.h"
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+
 using namespace std;
+
+void WriteTestResults(const string& filename, const string& testName, bool result) 
+{
+    ofstream logFile;
+    logFile.open(filename, ios::app); // Opens the file in append mode
+    if (logFile.is_open()) 
+    {
+        logFile << testName << " " << (result ? "Successful" : "Failed") << endl;
+        logFile.close();
+
+    }
+    else 
+    {
+        cout << "Unable to open log file!" << endl;
+    }
+}
 
 int main()
 {
+    // Current  date  and time
+    time_t currentTime = time(nullptr);
+    struct tm* localTime = localtime(&currentTime);
+
+    // Open the log file 
+    ofstream logFile;
+    logFile.open("test_log.txt", ios::app); //Opens the file in append mode
+    if (logFile.is_open()) 
+    {
+       // Writes the date and time to the log file
+        logFile << "Date: " << put_time(localTime, "%x") << " Time: " << put_time(localTime, "%X") << endl;
+    }
+
+    // Run tests and write the results to the log file
+    int totalTests = 0;
+    int passedTests = 0;
+
     // Test 1: Output "Hello, World" to the console
+    totalTests++;
     String str1 = "Hello, World";
     str1.WriteToConsole();
     cout << endl;
+    passedTests++;
+    WriteTestResults("test_log.txt", "Test 1", true);
+    
+    /*String str1 = "Hello, World";
+    str1.WriteToConsole();
+    cout << endl;*/
 
-    // Testing all the functions and operators
-    String str2 = "Hello";
-    String str3 = "World";
-    String str4;
+    //// Testing all the functions and operators
+    //String str2 = "Hello";
+    //String str3 = "World";
+    //String str4;
 
     // Testing Append
+    totalTests++;
+    String str2 = "Hello";
+    String str3 = "World";
     str2.Append(", ").Append(str3);
+    cout << "Appended String: ";
     str2.WriteToConsole();
     cout << endl;
+    if (str2.EqualTo("Hello, World")) {
+        passedTests++;
+        WriteTestResults("test_log.txt", "Test 2 Append", true);
+    }
+    else {
+        WriteTestResults("test_log.txt", "Test 2 Append", false);
+    }
+    
+    /*str2.Append(", ").Append(str3);
+    str2.WriteToConsole();
+    cout << endl;*/
 
     // Testing Prepend
     str3.Prepend("Hello, ");
